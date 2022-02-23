4f8d48f9ccd0918b9eba6127ba792125db5a252c
Kmamrutkar91 <60057262+Kmamrutkar91@users.noreply.github.com>
2022-02-23 15:56:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,120 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const app = express()
+app.use(express.json())
+
+const customers = [
+    { firstName: 'John', age: 27 },
+    { firstName: 'James', age: 32 },
+    { firstName: 'Robert', age: 45 },
+    { firstName: 'Kalyani', age: 21 },
+]
+
+app.get('/', (req, resp) => {
+    resp.send("Welcome to Customers API")
+})
+
+app.get('/customers', (req, resp) => {
+    resp.send(customers)
+})
+
+app.post('/customers/login', (req, resp) => {
+    const user = {
+        name: "admin",
+        email: "admin@gmail.com"
+    };
+    const token = jwt.sign({ user }, 'mysecretkey');
+    resp.json({
+        token: token
+    });
+})
+
+app.get('/customers/:age', verifyToken, (req, resp) => {
+    const customer = customers.find(k => k.age === parseInt(req.params.age))
+    jwt.verify(req.token, 'mysecretkey', (err) => {
+        if (err) {
+            resp.status(404).send("Can not get customer by age")
+        }
+        else {
+            if (!customer) resp.status(404).send("Customer not found")
+            resp.send(customer)
+        }
+    })
+})
+
+app.get('/findFirstName/:firstName', verifyToken, (req, resp) => {
+    const customername = customers.find(k => k.firstName === (req.params.firstName))
+    jwt.verify(req.token, 'mysecretkey', (err) => {
+        if (err) {
+            resp.status(404).send("Can not get customer by name")
+        }
+        else {
+            if (!customername) resp.status(404).send("Customer not found")
+            resp.send(customername)
+        }
+    })
+})
+
+app.post('/customers/addCustomer', verifyToken, (req, resp) => {
+    const newCustomer = {
+        firstName: req.body.firstName,
+        age: req.body.age
+    }
+    jwt.verify(req.token, 'mysecretkey', (err) => {
+        if (err) {
+            resp.status(404).send("Can not add customer")
+        }
+        else {
+            customers.push(newCustomer)
+            resp.send(newCustomer)
+        }
+    })
+})
+
+app.put('/customers/:age', verifyToken, (req, resp) => {
+    const customer = customers.find(k => k.age === parseInt(req.params.age))
+    jwt.verify(req.token, 'mysecretkey', (err) => {
+        if (err) {
+            resp.status(404).send("Can not Update customer")
+        }
+        else {
+            if (!customer) resp.status(404).send("Customer not found")
+            else {
+                customer.firstName = req.body.firstName
+                resp.send(customer)
+            }
+
+        }
+    })
+})
+
+app.delete('/customers/:age', verifyToken, (req, resp) => {
+    const customer = customers.find(k => k.age === parseInt(req.params.age))
+    jwt.verify(req.token, 'mysecretkey', (err) => {
+        if (err) {
+            resp.status(404).send("Can not delete customer")
+        }
+        else {
+            if (!customer) resp.status(404).send("Customer not found")
+            else {
+                const index = customers.indexOf(customer)
+                customers.splice(index, 1)
+                resp.send(customer)
+            }
+        }
+    })
+})
+
+app.listen(3000);
+function verifyToken(req, resp, next) {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader != "undefined") {
+        const bearer = bearerHeader.split(' ')
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    }
+    else {
+        resp.status(404).send("Wrong Token")
+    }
+}
---
+++ mongodb.js
@@ -0,0 +1,122 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const MongoClient = require('mongodb').MongoClient
+const app = express()
+app.use(express.json())
+var database
+
+app.get('/', (req, resp) => {
+    resp.send("Welcome to mongodb API")
+})
+
+app.get('/products', (req, resp) => {
+    database.collection('products').find({}).toArray((err, result) => {
+        if (err) throw error
+        resp.send(result)
+    })
+})
+
+app.post('/products/login', (req, resp) => {
+    const user = {
+        name: "admin",
+        email: "admin@gmail.com"
+    };
+    const token = jwt.sign({ user }, 'mysecretkey');
+    resp.json({
+        token: token
+    });
+})
+
+app.get('/products/:id', verifyToken, (req, resp) => {
+    jwt.verify(req.token, 'mysecretkey', (err) => {
+        if (err) {
+            resp.status(404).send("Can not get product by id")
+        }
+        else {
+            database.collection('products').find({ id: parseInt(req.params.id) }).toArray((err, result) => {
+                if (err) throw err
+                resp.send(result)
+            })
+        }
+    })
+})
+
+app.post('/products/addProduct', verifyToken, (req, resp) => {
+    jwt.verify(req.token, 'mysecretkey', (err) => {
+        if (err) {
+            resp.status(404).send("Can not add product")
+        }
+        else {
+            let res = database.collection('products').find({}).sort({ id: -1 }).limit(1)
+            res.forEach(obj => {
+                if (obj) {
+                    let product = {
+                        id: obj.id + 1,
+                        name: req.body.product,
+                        price: req.body.price
+
+                    }
+                    database.collection('products').insertOne(product, (err, result) => {
+                        if (err) resp.status(500).send(err)
+                        resp.send("Added Successfully")
+                    })
+                }
+            })
+        }
+    })
+})
+app.put('/products/:id', verifyToken, (req, resp) => {
+    let query = { id: parseInt(req.params.id) }
+    let product = {
+        id: parseInt(req.params.id),
+        name: req.body.name
+    }
+    jwt.verify(req.token, 'mysecretkey', (err) => {
+        if (err) {
+            resp.status(404).send("Can not update product")
+        }
+        else {
+            let dataset = { $set: product }
+            database.collection('products').updateOne(
+                query,
+                dataset, (err, result) => {
+                    if (err) throw err
+                    resp.send(product)
+                })
+        }
+    })
+})
+
+app.delete('/products/:id', verifyToken, (req, resp) => {
+    jwt.verify(req.token, 'mysecretkey', (err) => {
+        if (err) {
+            resp.status(404).send("Can not delete product")
+        }
+        else {
+            database.collection('products').deleteOne({ id: parseInt(req.params.id) }, (err, result) => {
+                if (err) throw err
+                resp.send('Product is deleted')
+            })
+        }
+    })
+})
+
+app.listen(8080, () => {
+    MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, (error, result) => {
+        if (error) throw error
+        database = result.db('mydatabase')
+        console.log("CONEECTION SUCCESSFUL!!")
+    })
+})
+function verifyToken(req, resp, next) {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader != "undefined") {
+        const bearer = bearerHeader.split(' ')
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    }
+    else {
+        resp.status(404).send("Wrong Token")
+    }
+}
